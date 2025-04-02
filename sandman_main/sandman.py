db11ecdf525f725d8c6f5d36eb5de72c05c1acc9
Shawn Lindberg <shawn.lindberg@gmail.com>
2025-04-01 22:30:51 -0500

    Added a class to hold the application state and logic. Started implementing code to set up ensure that the base directory exists.

--- sandman_main/sandman.py
+++ sandman_main/sandman.py
@@ -2,6 +2,28 @@
 
 import logging
 import logging.handlers
+import pathlib
+
+
+class Sandman:
+    """The state and logic to run the Sandman application."""
+
+    def __init__(self) -> None:
+        """Initialize the instance."""
+        pass
+
+    def run(self) -> None:
+        """Run the program."""
+        self.__base_dir = str(pathlib.Path.home()) + "/.sandman/"
+
+        base_path = pathlib.Path(self.__base_dir)
+
+        base_dir_exists = base_path.exists()
+        if base_dir_exists == False:
+            pass
+
+        # logger = _setup_logging()
+        # logger.info("Starting Sandman...")
 
 
 def _setup_logging() -> logging.Logger:
@@ -29,5 +51,6 @@ def _setup_logging() -> logging.Logger:
     return logger
 
 
-_logger = _setup_logging()
-_logger.info("Starting Sandman...")
+if __name__ == "__main__":
+    sandman = Sandman()
+    sandman.run()
