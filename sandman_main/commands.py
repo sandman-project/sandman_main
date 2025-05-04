fdf5bf4d38d5003944d37ee4175ea85eca2967b5
Shawn Lindberg <shawn.lindberg@gmail.com>
2025-05-04 17:26:41 -0500

    Added code to parse move control intents into commands. Also slightly refactored overall intent parsing.

--- sandman_main/commands.py
+++ sandman_main/commands.py
@@ -1,7 +1,17 @@
 """All of the commands that can be processed."""
 
+import dataclasses
+
 
 class StatusCommand:
     """A command to get the status."""
 
     pass
+
+
+@dataclasses.dataclass
+class MoveControlCommand:
+    """A command to move a control."""
+
+    control_name: str
+    direction: str
