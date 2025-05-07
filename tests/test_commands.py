0c82288c1583cc05103fd7da65819bc9760e19b2
Shawn Lindberg <shawn.lindberg@gmail.com>
2025-05-06 22:17:00 -0500

    Added tests for malformed intents and the get status intent.

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -1,1 +1,24 @@
 """Tests commands."""
+
+import sandman_main.commands as commands
+
+
+def test_invalid_intents() -> None:
+    """Test invalid intent constructions."""
+    # Intents are expected to have an intent key.
+    assert commands.parse_from_intent({}) is None
+
+    # Which is expected to have an intentName key.
+    assert commands.parse_from_intent({"intent": {}}) is None
+
+    # Which should be a string.
+    assert commands.parse_from_intent({"intent": {"intentName": 1}}) is None
+    assert commands.parse_from_intent({"intent": {"intentName": ""}}) is None
+
+
+def test_get_status_intent() -> None:
+    """Test get status intent constructions."""
+    command = commands.parse_from_intent(
+        {"intent": {"intentName": "GetStatus"}}
+    )
+    assert isinstance(command, commands.StatusCommand)
