b4498732d5dd0dbce77dbe8d7bb28d1616ae6a21
Shawn Lindberg <shawn.lindberg@gmail.com>
2025-05-10 16:30:43 -0500

    Added controls and a test to verify that they start off in the idle state.

--- sandman_main/controls.py
+++ sandman_main/controls.py
@@ -1,2 +1,27 @@
-"""Everything needed to manage controls. Controls are used to manipulate parts 
-of the bed."""
+"""Everything needed to manage controls.
+
+Controls are used to manipulate parts of the bed.
+"""
+
+import enum
+import logging
+
+
+class ControlState(enum.Enum):
+    """The various states a control can be in."""
+
+    IDLE = 0
+
+
+class Control:
+    """The state and logic for a control that manages a part of the bed."""
+
+    def __init__(self, name: str) -> None:
+        """Initialize the instance."""
+        self.__logger = logging.getLogger("sandman.control." + name)
+        self.__state = ControlState.IDLE
+        self.__name = name
+
+    def get_state(self) -> ControlState:
+        """Get the current state."""
+        return self.__state
--- tests/test_controls.py
+++ tests/test_controls.py
@@ -1,1 +1,8 @@
 """Tests controls."""
+
+import sandman_main.controls as controls
+
+
+def test_control_initialization() -> None:
+    """Test control initialization."""
+    assert controls.Control("test1").get_state() == controls.ControlState.IDLE
