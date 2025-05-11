b48720db99a09bb2544e94424733b7c509799a26
Shawn Lindberg <shawn.lindberg@gmail.com>
2025-05-11 17:22:32 -0500

    Added the rest of the states and started working on a test for state transitions.

--- sandman_main/controls.py
+++ sandman_main/controls.py
@@ -7,10 +7,22 @@ import enum
 import logging
 
 
+@enum.unique
 class ControlState(enum.Enum):
     """The various states a control can be in."""
 
     IDLE = 0
+    MOVE_UP = 1
+    MOVE_DOWN = 2
+    COOL_DOWN = 3
+
+
+_state_names = [
+    "idle",  # IDLE
+    "move up",  # MOVE_UP
+    "move down",  # MOVE_DOWN
+    "cool down",  # COOL_DOWN
+]
 
 
 class Control:
@@ -26,6 +38,17 @@ class Control:
     def get_state(self) -> ControlState:
         """Get the current state."""
         return self.__state
+
+    def set_desired_state(self, state: ControlState) -> None:
+        """Set the next state."""
+        if state == ControlState.COOL_DOWN:
+            return
+
+        self.__desired_state = state
+
+        self.__logger.info(
+            "Set desired state to '%s'.", _state_names[state.value]
+        )
 
     def process(self) -> None:
         """Process the control."""
--- tests/test_controls.py
+++ tests/test_controls.py
@@ -12,3 +12,14 @@ def test_control_initialization() -> None:
     # It should remain idle without change.
     control.process()
     assert control.get_state() == controls.ControlState.IDLE
+
+
+def test_control_state_transitions() -> None:
+    """Test control transitioning between states."""
+    control = controls.Control("test2")
+    assert control.get_state() == controls.ControlState.IDLE
+
+    # There should be no state change after setting the desired state without
+    # processing.
+    control.set_desired_state(controls.ControlState.MOVE_UP)
+    assert control.get_state() == controls.ControlState.IDLE
