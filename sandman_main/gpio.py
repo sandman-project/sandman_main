f04a83eff2a0aef75e15dfd824cf54b3f101d768
Shawn Lindberg <shawn.lindberg@gmail.com>
2025-06-10 22:26:37 -0500

    Added the very beginnings of a GPIO manager class. The tests will currently only work on a Raspberry Pi 4.

--- sandman_main/gpio.py
+++ sandman_main/gpio.py
@@ -1,1 +1,25 @@
 """Manages GPIO, which is used by the controls to move the bed."""
+
+import logging
+
+import gpiod
+
+
+class GPIOManager:
+    """Manages access to GPIO functionality."""
+
+    def __init__(self) -> None:
+        """Initialize the instance."""
+        self.__logger = logging.getLogger("sandman.gpio_manager")
+        self.__chip = None
+
+        chip_path = "/dev/gpiochip0"
+
+        try:
+            self.__chip = gpiod.Chip(chip_path)
+
+        except OSError:
+            self.__logger.warning("Failed to open GPIO chip %s", chip_path)
+
+        # Cleanup on destruction?
+        self.__chip.close()
--- tests/test_gpio.py
+++ tests/test_gpio.py
@@ -1,1 +1,9 @@
 """Tests GPIO."""
+
+import sandman_main.gpio as gpio
+
+
+def test_gpio_initialization() -> None:
+    """Test GPIO initialization."""
+    manager = gpio.GPIOManager()
+    assert manager is not None
