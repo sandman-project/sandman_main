170a85fb3e1e577944d397a1c0235f64a402ed03
Adrian <73155005+adrian-javid@users.noreply.github.com>
2025-06-15 10:47:45 -0500

    change import

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,13 @@ from collections.abc import Generator
 
 import pytest
 
-import sandman_main.sandman as sandman_
+from sandman_main.sandman import Sandman, create_app
 
 
 @pytest.fixture
-def sandman() -> Generator[sandman_.Sandman]:
+def sandman() -> Generator[Sandman]:
     """Return a test app."""
-    app = sandman_.create_app({"BASE_DIR": "tests/data/", "TESTING": True})
+    app = create_app({"BASE_DIR": "tests/data/", "TESTING": True})
     if app is None:
         raise ValueError("failed to create app")
     yield app
