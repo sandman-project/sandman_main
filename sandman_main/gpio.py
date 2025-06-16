4ca26d0b539f6726c3f97e0d2f1ea9a8fd852eed
Shawn Lindberg <shawn.lindberg@gmail.com>
2025-06-15 22:25:45 -0500

    Split the opening and the closing of the GPIO chip into an initialize and uninitialize function respectively. Added type hints to variables for just the GPIO modules.

--- sandman_main/gpio.py
+++ sandman_main/gpio.py
@@ -10,30 +10,24 @@ class GPIOManager:
 
     def __init__(self) -> None:
         """Initialize the instance."""
-        self.__logger = logging.getLogger("sandman.gpio_manager")
-        self.__chip = None
+        self.__logger: logging.Logger = logging.getLogger(
+            "sandman.gpio_manager"
+        )
+        self.__chip: gpiod.Chip | None = None
 
-        chip_path = "/dev/gpiochip0"
+    def initialize(self) -> None:
+        """Set up the manager for use."""
+        chip_path: str = "/dev/gpiochip0"
 
         try:
             self.__chip = gpiod.Chip(chip_path)
 
         except OSError:
             self.__logger.warning("Failed to open GPIO chip %s", chip_path)
-
-        if self.__chip is None:
             return
 
-        with self.__chip.request_lines(
-            consumer="sandman-test",
-            config={
-                20: gpiod.LineSettings(
-                    direction=gpiod.line.Direction.OUTPUT,
-                    output_value=gpiod.line.Value.ACTIVE,
-                )
-            },
-        ) as request:
-            request.set_value(20, gpiod.line.Value.ACTIVE)
-
-        # Cleanup on destruction?
-        self.__chip.close()
+    def uninitialize(self) -> None:
+        """Clean up the manager after use."""
+        if self.__chip is not None:
+            self.__chip.close()
+            self.__chip = None
--- tests/test_gpio.py
+++ tests/test_gpio.py
@@ -5,5 +5,8 @@ import sandman_main.gpio as gpio
 
 def test_gpio_initialization() -> None:
     """Test GPIO initialization."""
-    manager = gpio.GPIOManager()
+    manager: gpio.GPIOManager = gpio.GPIOManager()
     assert manager is not None
+
+    manager.initialize()
+    manager.uninitialize()
