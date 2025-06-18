9b0347a3a0e03892a027d04715a580aa275953e1
Shawn Lindberg <shawn.lindberg@gmail.com>
2025-06-17 22:27:01 -0500

    Started adding the ability to acquire GPIO lines for output. Added a way to access a list of lines that have been acquired.

--- sandman_main/gpio.py
+++ sandman_main/gpio.py
@@ -1,4 +1,7 @@
-"""Manages GPIO, which is used by the controls to move the bed."""
+"""Manages GPIO, which is used by the controls to move the bed.
+
+NOTE - Acquires exclusive access to GPIO lines.
+"""
 
 import logging
 
@@ -14,6 +17,7 @@ class GPIOManager:
             "sandman.gpio_manager"
         )
         self.__chip: gpiod.Chip | None = None
+        self.__line_requests: dict[int, gpiod.line_request.LineRequest] = {}
 
     def initialize(self) -> None:
         """Set up the manager for use."""
@@ -31,3 +35,15 @@ class GPIOManager:
         if self.__chip is not None:
             self.__chip.close()
             self.__chip = None
+
+    @property
+    def acquired_lines(self) -> list[int]:
+        """Access a list of the acquired lines."""
+        return list(self.__line_requests)
+
+    def acquire_output_line(self, line: int) -> bool:
+        """Acquire a line for output."""
+        if self.__chip is None:
+            return False
+
+        return False
--- tests/test_gpio.py
+++ tests/test_gpio.py
@@ -3,10 +3,34 @@
 import sandman_main.gpio as gpio
 
 
+class TestGPIOManager(gpio.GPIOManager):
+    """A special-purpose manager for use with testing."""
+
+    # Despite its name, this class should not be collected for testing.
+    __test__ = False
+
+    def __init__(self) -> None:
+        """Initialize the instance."""
+        super().__init__()
+
+
 def test_gpio_initialization() -> None:
     """Test GPIO initialization."""
-    manager: gpio.GPIOManager = gpio.GPIOManager()
+    manager: TestGPIOManager = TestGPIOManager()
     assert manager is not None
+    assert len(manager.acquired_lines) == 0
 
     manager.initialize()
+    assert len(manager.acquired_lines) == 0
+
     manager.uninitialize()
+    assert len(manager.acquired_lines) == 0
+
+
+def test_gpio_acquire_lines() -> None:
+    """Test acquiring GPIO lines."""
+    manager: TestGPIOManager = TestGPIOManager()
+
+    # Cannot acquire lines prior to initialization.
+    assert manager.acquire_output_line(1) == False
+    assert len(manager.acquired_lines) == 0
