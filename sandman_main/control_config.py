fced8e4deb83e5621dbc5e31e24e59942f4e9a6b
Shawn Lindberg <shawn.lindberg@gmail.com>
2025-07-27 21:28:12 -0500

    Added the very beginnings of a class for control configs and a nearly meaningless test.

--- sandman_main/control_config.py
+++ sandman_main/control_config.py
@@ -1,1 +1,14 @@
 """Provides configuration for controls."""
+
+
+class ControlConfig:
+    """Specifies the configuration of a control."""
+
+    def __init__(self) -> None:
+        """Initialize the control config."""
+        self.__name: str = ""
+
+    @property
+    def name(self) -> str:
+        """Get the name."""
+        return self.__name
--- tests/test_control_config.py
+++ tests/test_control_config.py
@@ -1,1 +1,9 @@
 """Tests configuration for controls."""
+
+import sandman_main.control_config as control_config
+
+
+def test_control_config_initialization() -> None:
+    """Test control config initialization."""
+    config = control_config.ControlConfig()
+    assert config.name == ""
