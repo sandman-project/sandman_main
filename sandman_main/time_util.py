fa8a6e654446eb134c4288ea7be3eb3f7cbe196b
Shawn Lindberg <shawn.lindberg@gmail.com>
2025-09-23 22:19:55 -0500

    Added an initial implementation of the time source. Also implemented a test time source, but it's incomplete.

--- sandman_main/time_util.py
+++ sandman_main/time_util.py
@@ -1,1 +1,22 @@
 """Useful things for dealing with time."""
+
+import tzlocal
+import whenever
+
+
+class TimeSource:
+    """An interface for getting the current time."""
+
+    def __init__(self) -> None:
+        """Initialize the instance."""
+        pass
+
+    def get_current_time_zone_name(self) -> str:
+        """Get the name of the current time zone."""
+        return tzlocal.get_localzone_name()
+
+    def get_current_time(self) -> whenever.ZonedDateTime:
+        """Get the current time in the current time zone."""
+        time_zone_name = self.get_current_time_zone_name()
+        global_time = whenever.Instant.now()
+        return global_time.to_tz(time_zone_name)
--- tests/test_time_util.py
+++ tests/test_time_util.py
@@ -1,1 +1,42 @@
 """Tests time utilities."""
+
+import typing
+
+import whenever
+
+import sandman_main.time_util as time_util
+
+_default_time = whenever.ZonedDateTime(
+    year=2025, month=9, day=23, hour=21, minute=42, tz="America/Chicago"
+)
+
+
+class TestTimeSource(time_util.TimeSource):
+    """A special-purpose time source for use with testing."""
+
+    # Despite its name, this class should not be collected for testing.
+    __test__ = False
+
+    @typing.override
+    def __init__(self) -> None:
+        """Initialize the instance."""
+        super().__init__()
+        self.__time_zone_name = _default_time.tz
+        self.__curr_time = _default_time.to_instant()
+
+    @typing.override
+    def get_current_time_zone_name(self) -> str:
+        """Get the name of the current time zone."""
+        return self.__time_zone_name
+
+    @typing.override
+    def get_current_time(self) -> whenever.ZonedDateTime:
+        """Get the current time in the current time zone."""
+        return self.__curr_time.to_tz(self.get_current_time_zone_name())
+
+
+def test_time_source() -> None:
+    """Test the test time source."""
+    time_source = TestTimeSource()
+    assert time_source.get_current_time_zone_name() == _default_time.tz
+    assert time_source.get_current_time() == _default_time
