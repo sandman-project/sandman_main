63caf9a6b2351d953047abd7e03e91c90c23af4b
Shawn Lindberg <shawn.lindberg@gmail.com>
2025-09-25 22:00:21 -0500

    Added functionality to change the current time in the test time source.

--- tests/test_time_util.py
+++ tests/test_time_util.py
@@ -34,9 +34,24 @@ class TestTimeSource(time_util.TimeSource):
         """Get the current time in the current time zone."""
         return self.__curr_time.to_tz(self.get_current_time_zone_name())
 
+    def set_current_time(self, new_time: whenever.ZonedDateTime) -> None:
+        """Set the current time and time zone."""
+        self.__time_zone_name = new_time.tz
+        self.__curr_time = new_time.to_instant()
+
 
 def test_time_source() -> None:
     """Test the test time source."""
     time_source = TestTimeSource()
     assert time_source.get_current_time_zone_name() == _default_time.tz
     assert time_source.get_current_time() == _default_time
+
+    new_time = whenever.ZonedDateTime(
+        year=2025, month=9, day=25, hour=21, minute=51, tz="America/New_York"
+    )
+
+    assert time_source.get_current_time_zone_name() != new_time.tz
+    assert time_source.get_current_time() != new_time
+    time_source.set_current_time(new_time)
+    assert time_source.get_current_time_zone_name() == new_time.tz
+    assert time_source.get_current_time() == new_time
