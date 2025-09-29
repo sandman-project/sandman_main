55e22b00500b6239bda0ba9b42650caa922d8cb0
Shawn Lindberg <shawn.lindberg@gmail.com>
2025-09-28 21:39:31 -0500

    Started making the report manager. Started making the tests for file creation. Mostly just preconditions so far.

--- sandman_main/report.py
+++ sandman_main/report.py
@@ -6,7 +6,20 @@ Reports are automatically generated based on activity.
 import logging
 import pathlib
 
+from . import time_util
+
 _logger = logging.getLogger("sandman.report")
+
+
+class ReportManager:
+    """Manages recording events into per day report files."""
+
+    def __init__(
+        self, time_source: time_util.TimeSource, base_dir: str
+    ) -> None:
+        """Initialize the instance."""
+        self.__time_source = time_source
+        self.__reports_dir = base_dir + "reports/"
 
 
 def bootstrap_reports(base_dir: str) -> None:
--- tests/test_report.py
+++ tests/test_report.py
@@ -2,7 +2,45 @@
 
 import pathlib
 
+import whenever
+
 import sandman_main.report as report
+import tests.test_time_util as test_time_util
+
+
+def _get_num_files_in_dir(path: pathlib.Path) -> int:
+    """Get the number of files in a given directory."""
+    num_files = 0
+
+    for child in path.iterdir():
+        if child.is_file():
+            num_files += 1
+
+    return num_files
+
+
+def test_report_file_creation(tmp_path: pathlib.Path) -> None:
+    """Test the creation of report files."""
+    report_path = tmp_path / "reports/"
+    report.bootstrap_reports(str(tmp_path) + "/")
+    assert report_path.exists() == True
+
+    time_source = test_time_util.TestTimeSource()
+
+    first_time = whenever.ZonedDateTime(
+        year=2025,
+        month=9,
+        day=28,
+        hour=16,
+        minute=59,
+        second=59,
+        tz="America/Chicago",
+    )
+    time_source.set_current_time(first_time)
+    assert time_source.get_current_time() == first_time
+
+    assert _get_num_files_in_dir(report_path) == 0
+    # report_manager = report.ReportManager(time_source, str(tmp_path) + "/")
 
 
 def test_report_bootstrap(tmp_path: pathlib.Path) -> None:
