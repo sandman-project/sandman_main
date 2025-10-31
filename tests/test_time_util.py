5135a83419f985e18105b60cacc51d6d808061ae
Shawn Lindberg <shawn.lindberg@gmail.com>
2025-10-30 21:24:38 -0500

    Changed TimeSource to no longer get the system time zone. Instead the time zone is initialized to an invalid value and must be set to something valid in order to use the time source.

--- tests/test_time_util.py
+++ tests/test_time_util.py
@@ -2,6 +2,7 @@
 
 import typing
 
+import pytest
 import whenever
 
 import sandman_main.time_util as time_util
@@ -21,37 +22,42 @@ class TestTimeSource(time_util.TimeSource):
     def __init__(self) -> None:
         """Initialize the instance."""
         super().__init__()
-        self.__time_zone_name = _default_time.tz
         self.__curr_time = _default_time.to_instant()
-
-    @typing.override
-    def get_current_time_zone_name(self) -> str:
-        """Get the name of the current time zone."""
-        return self.__time_zone_name
 
     @typing.override
     def get_current_time(self) -> whenever.ZonedDateTime:
         """Get the current time in the current time zone."""
-        return self.__curr_time.to_tz(self.get_current_time_zone_name())
+        time_zone_name = self.get_time_zone_name()
+        return self.__curr_time.to_tz(time_zone_name)
 
     def set_current_time(self, new_time: whenever.ZonedDateTime) -> None:
         """Set the current time and time zone."""
-        self.__time_zone_name = new_time.tz
+        self.set_time_zone_name(new_time.tz)
         self.__curr_time = new_time.to_instant()
 
 
 def test_time_source() -> None:
     """Test the test time source."""
     time_source = TestTimeSource()
-    assert time_source.get_current_time_zone_name() == _default_time.tz
+
+    # We start out with an invalid time zone.
+    assert time_source.get_time_zone_name() == ""
+    with pytest.raises(whenever.TimeZoneNotFoundError):
+        time_source.get_current_time()
+
+    with pytest.raises(ValueError):
+        time_source.set_time_zone_name("America")
+
+    time_source.set_time_zone_name(_default_time.tz)
+    assert time_source.get_time_zone_name() == _default_time.tz
     assert time_source.get_current_time() == _default_time
 
     new_time = whenever.ZonedDateTime(
         year=2025, month=9, day=25, hour=21, minute=51, tz="America/New_York"
     )
 
-    assert time_source.get_current_time_zone_name() != new_time.tz
+    assert time_source.get_time_zone_name() != new_time.tz
     assert time_source.get_current_time() != new_time
     time_source.set_current_time(new_time)
-    assert time_source.get_current_time_zone_name() == new_time.tz
+    assert time_source.get_time_zone_name() == new_time.tz
     assert time_source.get_current_time() == new_time
