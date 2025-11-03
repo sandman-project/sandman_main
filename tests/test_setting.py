e51a1298041bcfa93fcf50fede6f6b9ec5aa9e7c
Shawn Lindberg <shawn.lindberg@gmail.com>
2025-11-02 21:13:43 -0600

    Started adding tests for settings.

--- tests/test_setting.py
+++ tests/test_setting.py
@@ -1,1 +1,9 @@
 """Tests settings."""
+
+import pathlib
+
+
+def test_setting_bootstrap(tmp_path: pathlib.Path) -> None:
+    """Test setting bootstrapping."""
+    setting_path = tmp_path / "settings.cfg"
+    assert setting_path.exists() == False
