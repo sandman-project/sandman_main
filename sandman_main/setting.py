5b85556be26940e4935658d44f832c3d42c6c427
Shawn Lindberg <shawn.lindberg@gmail.com>
2025-11-04 22:28:07 -0600

    Added the Settings class and code to get/set the time zone name. Started adding tests for initialization and bootstrapping.

--- sandman_main/setting.py
+++ sandman_main/setting.py
@@ -1,1 +1,44 @@
 """Provides support for overall settings, not specific to a system."""
+
+import logging
+import pathlib
+import zoneinfo
+
+_logger = logging.getLogger("sandman.settings")
+
+
+class Settings:
+    """Specifies the overall settings."""
+
+    def __init__(self) -> None:
+        """Initialize the control config."""
+        self.__time_zone_name: str = ""
+
+    @property
+    def time_zone_name(self) -> str:
+        """Get the time zone name."""
+        return self.__time_zone_name
+
+    @time_zone_name.setter
+    def time_zone_name(self, time_zone_name: str) -> None:
+        """Set the time zone name."""
+        if isinstance(time_zone_name, str) == False:
+            raise TypeError("Time zone name must be a string.")
+
+        try:
+            _zone_info = zoneinfo.ZoneInfo(time_zone_name)
+
+        except Exception as exception:
+            raise ValueError("Invalid time zone name.") from exception
+
+        self.__time_zone_name = time_zone_name
+
+
+def bootstrap_settings(base_dir: str) -> None:
+    """Handle bootstrapping for settings."""
+    settings_path = pathlib.Path(base_dir + "settings.cfg")
+
+    if settings_path.exists() == True:
+        return
+
+    _logger.info("Creating missing settings file '%s'.", str(settings_path))
--- tests/test_setting.py
+++ tests/test_setting.py
@@ -2,8 +2,31 @@
 
 import pathlib
 
+import pytest
 
-def test_setting_bootstrap(tmp_path: pathlib.Path) -> None:
+import sandman_main.setting as setting
+
+
+def test_settings_initialization() -> None:
+    """Test settings initialization."""
+    settings = setting.Settings()
+    assert settings.time_zone_name == ""
+
+    with pytest.raises(TypeError):
+        settings.time_zone_name = 1
+    with pytest.raises(ValueError):
+        settings.time_zone_name = ""
+    with pytest.raises(ValueError):
+        settings.time_zone_name = "America"
+
+    settings.time_zone_name = "America/Chicago"
+    assert settings.time_zone_name == "America/Chicago"
+
+
+def test_settings_bootstrap(tmp_path: pathlib.Path) -> None:
     """Test setting bootstrapping."""
-    setting_path = tmp_path / "settings.cfg"
-    assert setting_path.exists() == False
+    settings_path = tmp_path / "settings.cfg"
+    assert settings_path.exists() == False
+
+    setting.bootstrap_settings(str(tmp_path) + "/")
+    # assert settings_path.exists() == True
