4dea6f5c9c02ecd504a55e47886702fdfc1bff31
Shawn Lindberg <shawn.lindberg@gmail.com>
2025-11-23 20:37:01 -0600

    Added bootstrapping for the routines directory.

--- sandman_main/routines.py
+++ sandman_main/routines.py
@@ -2,3 +2,29 @@
 
 Routines are user specified sequences of actions.
 """
+
+import logging
+import pathlib
+
+_logger = logging.getLogger("sandman.routines")
+
+
+def bootstrap_routines(base_dir: str) -> None:
+    """Handle bootstrapping for routines."""
+    routines_path = pathlib.Path(base_dir + "routines/")
+
+    if routines_path.exists() == True:
+        return
+
+    _logger.info(
+        "Creating missing routines directory '%s'.", str(routines_path)
+    )
+
+    try:
+        routines_path.mkdir()
+
+    except Exception:
+        _logger.warning(
+            "Failed to create routines directory '%s'.", str(routines_path)
+        )
+        return
--- tests/test_routines.py
+++ tests/test_routines.py
@@ -1,1 +1,14 @@
 """Tests routines."""
+
+import pathlib
+
+import sandman_main.routines as routines
+
+
+def test_routine_bootstrap(tmp_path: pathlib.Path) -> None:
+    """Test routine bootstrapping."""
+    reports_path = tmp_path / "routines/"
+    assert reports_path.exists() == False
+
+    routines.bootstrap_routines(str(tmp_path) + "/")
+    assert reports_path.exists() == True
